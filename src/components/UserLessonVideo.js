bf96162829bea0f07afdcc3f815e71fa4687996e
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-12-15 21:11:01 -0700

    removed dumb css styling from iframe.

--- src/components/UserLessonVideo.js
+++ src/components/UserLessonVideo.js
@@ -4,9 +4,7 @@ import { UserVideoContainer } from '../styles/components/VideoComponent.styles';
 const UserLessonVideo = ({ lesson }) => {
   console.log("lesson from video component: ", lesson)
   return (
-    <UserVideoContainer>
-      <iframe src={lesson.video} style={{'width':'100%','height':'550px','left':'0','top':'0', "border":"none", "pointerEvents": "none" }} allowFullScreen referrerPolicy='no-referrer-when-downgrade' title='Video Player'></iframe>
-    </UserVideoContainer>
+    <iframe src={lesson.video} style={{'width':'100%','height':'550px','left':'0','top':'0', "border":"none"}} allowFullScreen referrerPolicy='no-referrer-when-downgrade' title='Video Player'></iframe>
   )
 }
 
